a63ceac4953ab925232a845c63474cc3a1b7c9a6
RyanShuman <rysh4155@colorado.edu>
2017-12-02 17:22:32 -0700

    Add files via upload

---
+++ include/userdatabaseh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPasword = "";
+$dbName = "userdatabase";
+  
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPasword, $dbName);
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html>
+	<body>
+		<header>
+		<div>
+			<form action="include/signin.php" method="POST"> 
+			User ID: <input type = "text" name = user_id"/>
+			<br>
+			Password: <input type = "password" name = "password"/>
+			
+			<button type="submit" user_name=""></button>
+			</form>
+		</div>
+		</header>
+	</body>
+</html>
+ </DOCTYPE>
